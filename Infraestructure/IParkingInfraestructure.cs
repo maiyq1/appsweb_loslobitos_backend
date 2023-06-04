8ccf63ff03a261e57e7a1175ce55b1c652cb921c
Rendery Codes <48937882+Rendery@users.noreply.github.com>
2023-06-04 15:15:16 -0500

    Parking-E
    test

---
+++ Infraestructure/IParkingInfraestructure.cs
@@ -0,0 +1,14 @@
+﻿using Infraestructure.Models;
+
+namespace Infraestructure
+{
+    public interface IParkingInfraestructure
+    {
+        List<Parking> GetAll();
+        Parking GetById(int id);
+        bool Create(Parking input);
+        bool Update(int id, Parking input);
+        bool Delete(int id);
+        List<Reservation> GetReservationsByParking(int parkingId);
+    }
+}
